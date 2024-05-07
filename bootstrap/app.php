680e29f0ddfc87038f71f0adfeaafe0e1472dc91
Robert Amoah <mr_robertamoah@yahoo.com>
2024-05-07 23:17:51 +0100

    made some changes to correct unauthentication error when creating how-tos

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,9 +8,12 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        channels: __DIR__.'/../routes/channels.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+    )
+    ->withBroadcasting(
+        __DIR__.'/../routes/channels.php',
+        ['prefix' => 'api', 'middleware' => ['api', 'auth:sanctum']]
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
