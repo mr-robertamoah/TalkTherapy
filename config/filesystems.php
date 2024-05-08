9b2d93745fbe6244ff3942bf99948d9ff7bcba10
Robert Amoah <mr_robertamoah@yahoo.com>
2024-05-08 07:18:22 +0100

    changed filesystem config

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,8 +39,9 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => 'http://127.0.0.1:8000/storage',
-            // 'url' => env('APP_URL').'/storage',
+            // 'url' => 'http://127.0.0.1:8000/storage',
+            'url' => env('APP_URL').'/storage',
+            'scheme' => env('APP_SCHEME'),
             'visibility' => 'public',
             'throw' => false,
         ],
