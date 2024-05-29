13e762e1f92a224feae7f75f445875e78d34d185
Robert Amoah <mr_robertamoah@yahoo.com>
2024-05-29 07:02:21 +0100

    updated PerformGuardianshipLinkAction so you cannot become guardian multiple times

--- app/Actions/Link/PerformGuardianshipLinkAction.php
+++ app/Actions/Link/PerformGuardianshipLinkAction.php
@@ -4,12 +4,20 @@ namespace App\Actions\Link;
 
 use App\Actions\Action;
 use App\DTOs\CreateLinkDTO;
+use App\Exceptions\LinkException;
 use Illuminate\Support\Facades\Redirect;
 
 class PerformGuardianshipLinkAction extends Action
 {
     public function execute(CreateLinkDTO $createLinkDTO)
     {
+        if (
+            $createLinkDTO->user
+                ->wards()
+                ->where('ward_id', $createLinkDTO->link->for->id)
+                ->exists()
+        ) throw new LinkException("You are already a guardian of this user.", 422);
+
         $createLinkDTO->user->wards()->create(['ward_id' => $createLinkDTO->link->for->id]);
 
         return Redirect::route('profile.show');
