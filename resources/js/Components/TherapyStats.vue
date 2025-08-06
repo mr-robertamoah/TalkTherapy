c141596de5e3274b73c317e9a1ca2e829b6753f9
Robert Amoah <mr_robertamoah@yahoo.com>
2025-08-06 23:37:25 +0000

    Add therapy detail and information components
    Create specialized components for therapy details, payment info, statistics, and other details with modern card-based design

---
+++ resources/js/Components/TherapyStats.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bg-white p-6 shrink-0 w-full" id="therapy_stats">
+    <div class="text-gray-600 tracking-wide font-semibold">Stats</div>
+    <div class="my-4">
+      <ActivityBadge
+        :name="'total sessions'"
+        :value="therapy.sessionsCreated"
+        class="my-2 ml-6 mt-4"
+      />
+      <ActivityBadge
+        :name="'held sessions'"
+        :value="therapy.sessionsHeld"
+        class="my-2 mt-4"
+      />
+      <ActivityBadge
+        :name="'free sessions held'"
+        :value="therapy.freeSessions"
+        class="my-2 mt-4 ml-6"
+      />
+      <ActivityBadge
+        :name="'paid sessions held'"
+        :value="therapy.paidSessions"
+        class="my-2 mt-4"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ActivityBadge from '@/Components/ActivityBadge.vue'
+
+defineProps({
+  therapy: { default: null },
+})
+</script>
